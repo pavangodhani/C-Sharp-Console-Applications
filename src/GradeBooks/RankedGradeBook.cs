f0558c611006d1b66972dc912abeb2fce2e70a0b
Pavan <pavangodhani@gmail.com>
2021-04-18 11:52:56 +0530

    In the RankedGradeBook class creating an override for the GetLetterGrade method

--- src/GradeBooks/RankedGradeBook.cs
+++ src/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using GradeBook.Enums;
+using GradeBook.Students;
 
 namespace GradeBook.GradeBooks
 {
@@ -12,5 +13,47 @@ namespace GradeBook.GradeBooks
         {
             this.Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGarde)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+
+            IEnumerable<Student> studentsQuery = Students.OrderByDescending(x => x.AverageGrade);
+
+            var studentAverageGrades = new List<double>();
+
+
+            foreach (var student in studentsQuery)
+            {
+                studentAverageGrades.Add(student.AverageGrade);
+            }
+
+            var top20 = Convert.ToInt32(this.Students.Count * 0.2);
+
+            if (studentAverageGrades.IndexOf(averageGarde) < top20)
+            {
+                return 'A';
+            }
+            else if (studentAverageGrades.IndexOf(averageGarde) < 2 * top20)
+            {
+                return 'B';
+            }
+            else if (studentAverageGrades.IndexOf(averageGarde) < 3 * top20)
+            {
+                return 'C';
+            }
+            else if (studentAverageGrades.IndexOf(averageGarde) < 4 * top20)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
     }
 }
